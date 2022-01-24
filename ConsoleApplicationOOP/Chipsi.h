e3feda9a49d1fab8c4a8168fb8fe7648828591e5
Greench <mrartemtitenko@gmail.com>
2022-01-24 17:32:34 +0200

    kek

--- ConsoleApplicationOOP/Chipsi.h
+++ ConsoleApplicationOOP/Chipsi.h
@@ -1,1 +1,27 @@
 #pragma once
+#include "GoodBasic.h"
+
+class Chipsi : public GoodBasic
+{
+private:
+	int _weight;
+	string _typeOfTaste;
+public:
+	Chipsi();
+	Chipsi(int id, int price, string name, string description, int _weight, string _typeOfTaste);
+
+	int getWeight();
+
+	void setWeight(int newWeight);
+
+	string getType();
+
+	void setType(string newType);
+
+	
+
+
+
+
+};
+
